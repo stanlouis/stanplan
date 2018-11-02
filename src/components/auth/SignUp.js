725a7b7980cb24dbb75499fdd30ed3d3c3751b45
Stanley Louis <stanlouis@msn.com>
2018-11-02 07:37:33 -0400

    Add SignUp component

---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+export class SignUp extends Component {
+  state = {
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: ''
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <form className="white" onSubmit={this.handleSubmit}>
+          <h5 className="grey-text text-darken-3">Sign Up</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input type="text" id="password" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="firstName">FirstName</label>
+            <input type="text" id="firstName" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="lastName">LastName</label>
+            <input type="text" id="lastName" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
